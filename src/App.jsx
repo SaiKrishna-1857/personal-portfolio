31111b7bd4d6e167899cf373e49516f9067c532d
SaiKrishna Sangeetha <saikrishna.sangeetha@gmail.com>
2024-08-28 11:49:22 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ function App() {
     <Router>
       <TopNav />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/work-experience" element={<WorkExperience />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/recent-works" element={<RecentWorks />} />
-        <Route path="/get-in-touch" element={<GetInTouch />} />
+        <Route path="/personal-portfolio" element={<Home />} />
+        <Route path="/personal-portfolio/work-experience" element={<WorkExperience />} />
+        <Route path="/personal-portfolio/projects" element={<Projects />} />
+        <Route path="/personal-portfolio/recent-works" element={<RecentWorks />} />
+        <Route path="/personal-portfolio/get-in-touch" element={<GetInTouch />} />
       </Routes>
     </Router>
   );
