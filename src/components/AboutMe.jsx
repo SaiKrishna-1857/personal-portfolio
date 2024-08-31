4efd417534cc63b99de09940ad768b48e01eada9
SaiKrishna Sangeetha <saikrishna.sangeetha@gmail.com>
2024-08-30 23:16:16 -0400

    Updated about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,53 +1,57 @@
 import React from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
 import styles from "../AboutMe.module.css";
 
 function AboutMe() {
     return (
         <div className={styles.aboutContainer}>
-            <div className={styles.aboutSection}>
-                <h2>About Me</h2>
-                <p className={styles.aboutDescription}>
-                    Hello! I'm Sai Krishna Sangeetha, a passionate Full Stack Developer with experience in building scalable web applications. My journey in tech started with a fascination for how things work, leading me to pursue a Master's in Computer Science. I'm always eager to learn and experiment with new technologies.
-                </p>
+            <div className={styles.leftColumn}>
+                <div className={styles.aboutSection}>
+                    <h2>Hello! I'm Sai Krishna Sangeetha,</h2>
+                    <p className={styles.aboutDescription}>
+                        A passionate Full Stack Developer with experience in building scalable web applications. My journey in tech started with a fascination for how things work, leading me to pursue a Master's in Computer Science. I'm always eager to learn and experiment with new technologies. Apart from tech, I enjoy watching TV Shows, Cooking, Traveling, and playing outdoor sports, particularly Cricket or Badminton.
+                    </p>
+                </div>
+
+                <div className={styles.hobbiesSection}>
+                    <h2>Hobbies</h2>
+                    <ul className={styles.hobbiesList}>
+                        <li>Cooking</li>
+                        <li>Watching TV Shows</li>
+                        <li>Playing Cricket, Badminton</li>
+                        <li>Coding Personal Projects</li>
+                    </ul>
+                </div>
             </div>
 
-            <div className={styles.interestsSection}>
-                <h2>Personal Interests</h2>
-                <p className={styles.interestsDescription}>
-                    I love exploring the intersection of technology and agriculture, which led me to work on projects focused on soil analysis and precision farming. Apart from tech, I enjoy photography, traveling, and diving into the world of books, particularly science fiction and technology.
-                </p>
-            </div>
+            <div className={styles.rightColumn}>
+                <div className={styles.skillsSection}>
+                    <h2>Technical Skills</h2>
+                    <ul className={styles.skillsList}>
+                        <li>JavaScript (Angular, React, Node.js)</li>
+                        <li>Python (Django, Flask, FastAPI)</li>
+                        <li>GraphQL, RESTful APIs</li>
+                        <li>Docker, Kubernetes, AWS</li>
+                        <li>MySQL, MongoDB, DynamoDB</li>
+                        <li>CI/CD, Jenkins, GitHub Actions</li>
+                    </ul>
+                </div>
 
-            <div className={styles.skillsSection}>
-                <h2>Technical Skills</h2>
-                <ul className={styles.skillsList}>
-                    <li>JavaScript (React, Node.js)</li>
-                    <li>Python (Django, Flask)</li>
-                    <li>GraphQL, RESTful APIs</li>
-                    <li>Docker, Kubernetes, AWS</li>
-                    <li>MySQL, MongoDB, DynamoDB</li>
-                    <li>CI/CD, Jenkins, GitHub Actions</li>
-                </ul>
-            </div>
-
-            <div className={styles.hobbiesSection}>
-                <h2>Hobbies</h2>
-                <ul className={styles.hobbiesList}>
-                    <li>Photography</li>
-                    <li>Traveling</li>
-                    <li>Reading Sci-Fi Books</li>
-                    <li>Coding Personal Projects</li>
-                </ul>
-            </div>
-
-            <div className={styles.certificationsSection}>
-                <h2>Certifications</h2>
-                <ul className={styles.certificationsList}>
-                    <li>AWS Certified Solutions Architect – Associate</li>
-                    <li>Certified Kubernetes Administrator (CKA)</li>
-                    <li>Google Cloud Certified Professional Data Engineer</li>
-                    <li>Certified Django Developer</li>
-                </ul>
+                <div className={styles.certificationsSection}>
+                    <h2>Certifications</h2>
+                    <ul className={styles.certificationsList}>
+                        <li>
+                          <a href="https://www.credly.com/badges/f0777851-cd16-46a4-893b-45470ce8d548/public_url" target="_blank" rel="noopener noreferrer" className={styles.certificationLink}>
+                            AWS Certified Solutions Architect – Associate <FaExternalLinkAlt className={styles.externalLinkIcon} />
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.freecodecamp.org/certification/SaiKrishna1857/responsive-web-design" target="_blank" rel="noopener noreferrer" className={styles.certificationLink}>
+                            Certified Frontend Developer-FreeCodeCamp <FaExternalLinkAlt className={styles.externalLinkIcon} />
+                          </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -18,10 +18,10 @@ function WorkExperience() {
           <h3 className={styles.jobTitle}>Senior Full Stack Developer</h3>
           <p className={styles.companyName}>AgWise LLC., Mar 2024-Present</p>
           <p className={styles.jobDescription}>
-            Developed scalable web apps using Angular, Django, and MySQL, ensuring optimal performance, security, and user experience.
+            Developed scalable web apps using Angular, Django, ensuring optimal performance, security, and user experience.
           </p>
           <p className={styles.jobDescription}>
-            Improved response time of the application by mgrating from REST to GraphQL, enabling efficient and flexible data retrieval.
+            Improved response time of the application by mgrating from REST to GraphQL, enabling efficient data retrieval.
           </p>
           <p className={styles.jobDescription}>
             Architected and implemented an ETL system utilizing AWS Serverless Architecture to process customer CSV files and generate data-driven recommendations, increasing revenue by 30%. 
@@ -29,9 +29,9 @@ function WorkExperience() {
           <p className={styles.jobDescription}>
             Pioneered CI/CD pipeline development using Bit Bucket, AWS CodePipeline, automating build uploads and cache invalidations, reducing manual deployment efforts by 100%.
           </p>
-          <p className={styles.jobDescription}>
+          {/* <p className={styles.jobDescription}>
             Collaborated with UI/UX developers to create responsive and accessible front-end components using Angular and Tailwind CSS.
-          </p>
+          </p> */}
         </div>
       </div>
 
@@ -46,13 +46,13 @@ function WorkExperience() {
           <h3 className={styles.jobTitle}>Graduate Research Assistant</h3>
           <p className={styles.companyName}>University of Central Florida, August 2023 - May 2024</p>
           <p className={styles.jobDescription}>
-            Developed a responsive UI using React and CSS Modules, incorporating dynamic dropdowns and real-time search displays to enhance UX. 
+            Developed a responsive UI using React and CSS Modules, incorporating dynamic dropdowns and real-time search displays. 
           </p>
           <p className={styles.jobDescription}> 
             Incorporated GraphQL endpoints with in a React framework to streamline and optimize data retrieval process.
           </p>
           <p className={styles.jobDescription}>
-            Utilized JML annotations to define formal specifications for critical programs, ensuring adherence to intended behaviour and functionality.
+            Utilized JML annotations to define formal specifications for critical programs, ensuring adherence to intended functionality.
           </p>
         </div>
       </div>
@@ -68,7 +68,7 @@ function WorkExperience() {
           <h3 className={styles.jobTitle}>Software Development Engineer</h3>
           <p className={styles.companyName}>Freshworks Inc., Jan 2019 - Jul 2022</p>
           <p className={styles.jobDescription}>
-            Launched 20+ RESTful APIs in Ruby on Rails, integrating Microservices, Redis, for scalable and responsive backend infrastructure. 
+            Launched 20+ RESTful APIs in Ruby on Rails, integrating Microservices, Redis, for scalable and responsive infrastructure. 
           </p>
           <p className={styles.jobDescription}>
             Deployed 45+ highly efficient serverless APIs using AWS Lambda and API Gateway, optimizing performance. 
@@ -77,7 +77,7 @@ function WorkExperience() {
             Developed and executed unit tests using RSpec, ensuring proper test coverage and functionality of new features. 
           </p>
           <p className={styles.jobDescription}>
-            Implemented automation test suite using Selenium WebDriver, which increased test coverage by 30% and improved test execution time by 50%. 
+            Implemented automation test suite using Selenium WebDriver, which increased test coverage &execution time by 50%. 
           </p>
         </div>
       </div>
